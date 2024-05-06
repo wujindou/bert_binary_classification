1ced3ad8d2bc4efdde45b3c3873f1bb4274883df
JackieWu <wujindou1@163.com>
2024-05-06 09:33:42 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -6,10 +6,13 @@ class NLPCCTaskDataSet(Dataset):
         self.mini_test = mini_test
         self.is_test = is_test
         self.reply_lens = []
-        self.dataset = self.load_json_data(filepath)
+        self.dataset = self.load_json_data(filepath) if 'jsonl' not in filepath else self.load_jsonl_data(filepath)
 
     def load_json_data(self, filename):
         return json.load(open(filename,'r',encoding='utf-8'))
+        
+    def load_jsonl_data(self, filename):
+        return [json.loads(line) for line in open(filename,'r',encoding='utf-8').readlines()]
 
     def __getitem__(self, index):
 
